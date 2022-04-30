2b0044c2211d7778047f5816cc763ec9380a35c2
binarytrance <ganeshan.dash@gmail.com>
2022-05-01 00:34:54 +0530

    added prettier, created IssuesList component and issues list query

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,14 +1,31 @@
-import { Link } from "react-router-dom";
+import { useQuery } from 'react-query';
+import { IssueItem } from './IssueItem';
 
 export default function IssuesList() {
+  const issuesQuery = useQuery(['issues'], () => fetch('/api/issues').then(res => res.json()));
+  console.log(issuesQuery.data);
   return (
     <div>
-      <h1>Issues List</h1>
-      <ul>
-        <li>
-          <Link to="/issue/1">Issue 1</Link>
-        </li>
-      </ul>
+      <h2>Issues List</h2>
+      {issuesQuery.isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <ul className='issues-list'>
+          {issuesQuery.data.map(issue => (
+            <IssueItem
+              key={issue.id}
+              title={issue.title}
+              number={issue.number}
+              assignee={issue.assignee}
+              commentCount={issue.comments.length}
+              createdBy={issue.createdBy}
+              createdDate={issue.createdDate}
+              status={issue.status}
+              labels={issue.labels}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { worker } from "@uidotdev/react-query-api";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { worker } from '@uidotdev/react-query-api';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-new Promise((res) => setTimeout(res, 100))
+const queryClient = new QueryClient();
+
+new Promise(res => setTimeout(res, 100))
   .then(() =>
     worker.start({
       quiet: true,
-      onUnhandledRequest: "bypass",
+      onUnhandledRequest: 'bypass'
     })
   )
   .then(() => {
     ReactDOM.render(
       <React.StrictMode>
-        <BrowserRouter>
-          <div className="container">
-            <App />
-          </div>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <div className='container'>
+              <App />
+            </div>
+          </BrowserRouter>
+        </QueryClientProvider>
       </React.StrictMode>,
-      document.getElementById("root")
+      document.getElementById('root')
     );
   });
