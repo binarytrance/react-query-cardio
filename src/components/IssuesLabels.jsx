104d7da147df907c8b5a37a83c5ada79cd7c3047
binarytrance <ganeshan.dash@gmail.com>
2023-03-13 01:40:42 +0530

    issues label comments for what to do next added

---
+++ src/components/IssuesLabels.jsx
@@ -0,0 +1,5 @@
+// all labels = useLabelsData => use for colors and label names
+// labels present in the issue from issuesQuery.data
+// dropdown with all labels
+// highlight selected labels
+// change labels array in useMutation `/api/issues/${issueNumber}`
